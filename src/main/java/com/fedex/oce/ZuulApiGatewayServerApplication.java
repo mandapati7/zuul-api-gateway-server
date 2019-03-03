d6c4186cd2ea32bbac959d280099358b53142deb
Gopikrishna Mandapati <mandapati.g.c@gmail.com>
2019-03-03 12:48:37 -0500

    first commit

---
+++ src/main/java/com/fedex/oce/ZuulApiGatewayServerApplication.java
@@ -0,0 +1,29 @@
+package com.fedex.oce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
+	}
+	
+	/*
+	 * @Bean pubic AlwaysSampler defaultSampler() { return new AlwaysSampler(); }
+	 */
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+}
---
+++ src/main/java/com/fedex/oce/ZuulLoggingFilter.java
@@ -0,0 +1,41 @@
+package com.fedex.oce;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+	
+	private final static Logger LOG = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		LOG.info(" request -> {} request - uri -> {}", request, request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
